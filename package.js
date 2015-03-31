78db93eacc98e5fc84d457be7ece7e284ad492eb
ahmetcetin <ahmet@cetin.info>
2015-03-31 22:00:46 +0300

    Version changed, added support for autoform 5

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'mpowaga:autoform-summernote',
-  summary: 'Summernote editor for aldeed:autoform',
-  version: '0.3.1',
-  git: 'https://github.com/mpowaga/meteor-autoform-summernote'
+  name: 'ahmetcetin:autoform-summernote',
+  summary: 'Summernote editor for aldeed:autoform 5',
+  version: '0.3.2',
+  git: 'https://github.com/ahmetcetin/meteor-autoform-summernote'
 });
 
 Package.onUse(function(api) {
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
 
   api.use([
     'templating',
-    'aldeed:autoform@4.2.2'
+    'aldeed:autoform@4.0.0 || 5.0.0'
   ], both);
   api.use('summernote:standalone@0.6.0_1', 'client');
 
